32de5902477a9003ba10785de7ecc6057cc452e0
Michael Telahun <mtm@trevi.et>
2022-12-04 10:29:21 +0300

    Revert "End Chapter 03"
    This reverts commit 355b919841718c772eb10f73e9202b93b08b0a77.

--- src/routes/health_check.rs
+++
@@ -1,5 +0,0 @@
-use actix_web::{HttpResponse, Responder, HttpRequest};
-
-pub async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
--- src/routes/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod health_check;
-pub mod subscriptions;
-
-pub use health_check::*;
-pub use subscriptions::*;
