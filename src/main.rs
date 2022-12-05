bbb0f8625c0562cd877f1f6c1f5b96ed92e9368d
Michael Telahun <mtm@trevi.et>
2022-12-05 09:48:27 +0300

    End of chapter 05

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -12,11 +11,13 @@ async fn main() -> std::io::Result<()> {
 
     // Panic if we can't read config
     let configuration = get_configuration().expect("Failed to read config");
-    let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Faile to connect to postgres");
-    let address = format!("localhost:{}", configuration.application_port);
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).expect("Failed to bind to local address");
     run(listener, connection_pool)?.await
 }
